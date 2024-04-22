c38305e559509c656005db51b59a17932dcf8de9
veerLakshya <109882583+veerLakshya@users.noreply.github.com>
2024-04-23 00:45:23 +0530

    Adding shorter code

--- Common Algos/Sliding window of size k .cpp
+++ Common Algos/Sliding window of size k .cpp
@@ -14,6 +14,17 @@ long long maxSubarraySumOfSizeK(vector<long long> v, long long k) {
     }
     return ans;
 }
+//shorter code for above algo-
+long long algo(vector<long long> v, long long k) {
+    long long n = v.size();
+    long long sum = 0,ans=-1e9;
+    for (int i = 0;i < n;i++){
+        sum += v[i];
+        if(i>=k)sum -= v[i - k];
+        if(i>=k-1)ans = max(ans, sum);
+    }
+    return ans;
+}
 /*
     *Step1 calculate sum of first k elements as our first window
     *Step2 initialize ans as sum
