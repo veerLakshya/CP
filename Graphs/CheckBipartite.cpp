9711910f2477245cd6841289d5ed4511677fd342
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-01-31 00:54:36 +0530

    Update CheckBipartite.cpp

--- Graphs/CheckBipartite.cpp
+++ Graphs/CheckBipartite.cpp
@@ -28,24 +28,5 @@ bool isBipartite(vector<vector<int>>& graph) {
     return true;
 }
 
-int main() {
-    int n, e;
-    cout << "Enter number of vertices and edges: ";
-    cin >> n >> e;
-    vector<vector<int>> graph(n);
-    cout << "Enter the edges (u v):\n";
-    for (int i = 0; i < e; ++i) {
-        int u, v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-    
-    if (isBipartite(graph)) {
-        cout << "The graph is bipartite.\n";
-    } else {
-        cout << "The graph is not bipartite.\n";
-    }
-    
-    return 0;
-}
+// finding max of shortest distances between any two nodes in  graph - // https://leetcode.com/problems/divide-nodes-into-the-maximum-number-of-groups/description/
+// https://www.geeksforgeeks.org/find-maximum-shortest-distance-in-each-component-of-a-graph/
