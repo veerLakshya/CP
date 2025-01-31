00c79323d605dd91b06fd3c335789696989f6856
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-01-31 21:59:22 +0530

    Update DSU.cpp

--- Graphs/DSU.cpp
+++ Graphs/DSU.cpp
@@ -34,31 +34,5 @@ public:
     }
 };
 
-int main() {
-    int n = 5; // Number of elements
-    UnionFind uf(n);
-    
-    cout << "Initially, all elements are separate sets." << endl;
-    for (int i = 0; i < n; i++) {
-        cout << "Element " << i << " is in set " << uf.findSet(i) << endl;
-    }
-    
-    cout << "\nPerforming union operations: " << endl;
-    uf.unionSet(0, 1);
-    uf.unionSet(1, 2);
-    
-    cout << "Set of element 0: " << uf.findSet(0) << endl;
-    cout << "Set of element 1: " << uf.findSet(1) << endl;
-    cout << "Set of element 2: " << uf.findSet(2) << endl;
-    cout << "Set of element 3: " << uf.findSet(3) << endl;
-    cout << "Set of element 4: " << uf.findSet(4) << endl;
-    
-    cout << "\nChecking if elements are in the same set:" << endl;
-    cout << "Are 0 and 2 in the same set? " << (uf.isSameSet(0, 2) ? "Yes" : "No") << endl;
-    cout << "Are 3 and 4 in the same set? " << (uf.isSameSet(3, 4) ? "Yes" : "No") << endl;
-    
-    uf.unionSet(3, 4);
-    cout << "\nAfter union(3,4), are 3 and 4 in the same set? " << (uf.isSameSet(3, 4) ? "Yes" : "No") << endl;
-    
-    return 0;
-}
+// *PROBLEMS for DSU* -
+// https://codeforces.com/contest/151/problem/D
